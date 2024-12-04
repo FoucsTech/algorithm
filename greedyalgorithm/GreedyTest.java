70fcef15eea88eaeed01c173b0959b35421157e0
xu.haoyu/徐浩宇_沪_开发 <haoyu.xu@51job.com>
2024-12-04 13:44:50 +0800

    greedy algorithm solution time complexity n^2,

---
+++ greedyalgorithm/GreedyTest.java
@@ -0,0 +1,11 @@
+package greedyalgorithm;
+
+/**
+ * 贪心算法
+ * 从局部寻找最优解
+ */
+public class GreedyTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ greedyalgorithm/Question455.java
@@ -0,0 +1,31 @@
+package greedyalgorithm;
+
+import java.util.Arrays;
+
+/**
+ * 假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+ * 对每个孩子 i，都有一个胃口值 g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j] 。
+ * 如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是满足尽可能多的孩子，并输出这个最大数值。
+ */
+public class Question455 {
+    /**
+     * 优先让胃口最小的孩子吃到能满足他胃口的最小饼干
+     */
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+        outLoop:
+        for (int k : g) {
+            for (int j = 0; j < s.length; j++) {
+                if (s[j] >= k) {
+                    //找到第一个大于g[i]饼干，就是当前小孩能满足他胃口的最小饼干
+                    count++;
+                    s[j] = 0; //认为这个饼干吃掉了
+                    continue outLoop;
+                }
+            }
+        }
+        return count;
+    }
+}
